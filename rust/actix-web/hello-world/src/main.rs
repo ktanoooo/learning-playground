30ba3d6064538358e0bf8fe599cc6628a85104c1
ktanoooo <ktanoooo1112@gmail.com>
2023-05-14 17:01:46 +0900

    Add actix-web playgraound

---
+++ rust/actix-web/hello-world/src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok.body("Hello world!")
+}
+
+#[post("/")]
+async fn echo(req_body: String) -> impl Responde {
+    HttpResponse::Ok.body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponder::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello).service(echo).route("/hey", web::get().(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run().await
+}
